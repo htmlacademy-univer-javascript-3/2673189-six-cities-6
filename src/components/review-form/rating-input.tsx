8433b0120594b2b43ea5feb015bb2138e17ecbb0
MaxD3mage <terzi.kirill@gmail.com>
2025-11-05 19:08:51 +0500

    fix review-form

---
+++ src/components/review-form/rating-input.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type RatingInputProps = {
+  value: number;
+  title: string;
+  name?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+export default function RatingInput({ value, title, name = 'rating', onChange }: RatingInputProps): JSX.Element {
+  const id = `${value}-stars`;
+  return (
+    <>
+      <input
+        className="form__rating-input visually-hidden"
+        onChange={onChange}
+        name={name}
+        value={value}
+        id={id}
+        type="radio"
+      />
+      <label htmlFor={id} className="reviews__rating-label form__rating-label" title={title}>
+        <svg className="form__star-image" width="37" height="33">
+          <use xlinkHref="#icon-star"></use>
+        </svg>
+      </label>
+    </>
+  );
+}
